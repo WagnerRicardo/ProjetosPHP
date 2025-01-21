425a84c48a9c02fba68f04857d68d6f05d1a8677
Wagner Ricardo de Paula Marcolongo <122945519+WagnerRicardo@users.noreply.github.com>
2025-01-21 12:17:10 -0300

    Update processaRequisicao.php
    Finalizando a aplicação App Send Mail, feedback visual e tratamento de exceções.

--- app_send_mail/processaRequisicao.php
+++ app_send_mail/processaRequisicao.php
@@ -13,6 +13,7 @@
         private $destino = null;
         private $assunto = null;
         private $mensagem = null;
+        private $status = ['codigo' => null, 'descricao' => ''];
 
         public function __get($attr){
             return $this->$attr;
@@ -24,6 +25,7 @@
 
         public function validaMensagem(){
             if(empty($this->destino) || empty($this->assunto) || empty($this->mensagem)){
+                $status['codigo'] = 2;
                 return false;
             }
             return true;
@@ -38,14 +40,14 @@
 
     if(!$msg->validaMensagem()){
         echo 'Informações invalidas';
-        die();
+        header('Location: index.php');
     }
 
     $mail = new PHPMailer(true);
 
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->SMTPDebug = false;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
@@ -73,8 +75,51 @@
         $mail->AltBody = $msg->__get('mensagem');
 
         $mail->send();
-        echo 'Mensagem enviada com sucesso!';
+
+        $status['codigo'] = 1;
+        $status['descricao'] = 'Mensagem foi enviada com sucesso para o e-mail: '. $msg->__get('destino') . '.';
+
     } catch (Exception $e) {
-        echo "Houve um Erro ao enviar o e-mail: {$mail->ErrorInfo}";
+
+        $status['codigo'] = 2;
+        $status['descricao'] = "Houve um Erro ao enviar o e-mail: {$mail->ErrorInfo}";
+
     }
 ?>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+    	<title>App Mail Send</title>
+
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+
+		<div class="container">  
+
+			<div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+
+            <div class="container">
+                <?php 
+                    if($status['codigo'] == 1){
+                        echo '<h1 class="display-4 text-success">Mensagem enviada com sucesso!</h1>';
+                        echo "<p>{$status['descricao']}</p>";
+                    }
+                    if($status['codigo'] == 2){
+                        echo '<h1 class="display-4 text-danger">Ops!</h1>';
+                        echo "<p>{$status['descricao']}</p>";
+                    }
+                
+                ?>
+                <a href="index.php" class="btn btn-info btn-lg mt-5 text-white">Voltar</a>
+            </div>
+
+	</body>
+</html>
