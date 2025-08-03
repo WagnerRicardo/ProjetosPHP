09f45ade046cda393a348168d6b3a7db2f0e63a6
Wagner Ricardo de Paula Marcolongo <122945519+WagnerRicardo@users.noreply.github.com>
2025-08-03 15:52:37 -0300

    Create index.php

---
+++ website/index.php
@@ -0,0 +1,10 @@
+<?php
+    $name = "Lucy Di Paula";
+    $age = 20;
+    $employed = true;
+    $height = 1.69;
+
+    echo "{$name} is {$age} years old, and is {$height}cm tall, ";
+    echo $employed ? 'is employed':'is not employed'
+    
+?>
